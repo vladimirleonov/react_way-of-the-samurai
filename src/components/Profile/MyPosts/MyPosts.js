9bee2e37835992e6fe44f6429ac367857a01ab9f
vladimir <vladimirleonov789@gmail.com>
2021-10-09 17:16:18 +0300

    used PureComponent

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,27 +3,39 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import NewPostForm from "./NewPostForm/NewPostForm";
 
-const MyPosts = (props) => {
-    const newPostData = props.postData.map((item) => {
+window.props = [];
+
+class MyPosts extends React.PureComponent {
+    newPostData = this.props.postData.map((item) => {
         return <Post key={item.id} text={item.text} likeCount={item.likeCount}/>
     })
 
-    const onSubmit = (values) => {
+    onSubmit = (values) => {
         const { newPostValue } = values;
         if(newPostValue) {
-            props.onAddPost(newPostValue);
+            this.props.onAddPost(newPostValue);
         }
     }
 
-    return (
-        <div className={s.my__posts}>
-            <div className={s.header}>My posts</div>
-            <NewPostForm onSubmit={onSubmit}/>
-            <div className={s.posts}>
-                {newPostData}
+  /*  shouldComponentUpdate(nextProps, nextState) {
+        return nextProps != this.props  ;
+        // return false;
+    }*/
+
+    render () {
+        console.log(this.props);
+        {window.props.push(this.props)}
+
+        return (
+            <div className={s.my__posts}>
+                <div className={s.header}>My posts</div>
+                <NewPostForm onSubmit={this.onSubmit}/>
+                <div className={s.posts}>
+                    {this.newPostData}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default MyPosts;
