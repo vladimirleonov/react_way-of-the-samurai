1e6e0ae4a997ba79c7c99e64e2b5e9bf61e85ee3
vladimir <vladimirleonov789@gmail.com>
2021-10-09 17:22:32 +0300

    used React.memo() instead of PureComponent or shoulComponentUpdate()

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,39 +3,28 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import NewPostForm from "./NewPostForm/NewPostForm";
 
-window.props = [];
-
-class MyPosts extends React.PureComponent {
-    newPostData = this.props.postData.map((item) => {
+const MyPosts = React.memo((props) => {
+    const newPostData = props.postData.map((item) => {
         return <Post key={item.id} text={item.text} likeCount={item.likeCount}/>
     })
 
-    onSubmit = (values) => {
+    const onSubmit = (values) => {
         const { newPostValue } = values;
         if(newPostValue) {
-            this.props.onAddPost(newPostValue);
+            props.onAddPost(newPostValue);
         }
     }
 
-  /*  shouldComponentUpdate(nextProps, nextState) {
-        return nextProps != this.props  ;
-        // return false;
-    }*/
-
-    render () {
-        console.log(this.props);
-        {window.props.push(this.props)}
-
-        return (
-            <div className={s.my__posts}>
-                <div className={s.header}>My posts</div>
-                <NewPostForm onSubmit={this.onSubmit}/>
-                <div className={s.posts}>
-                    {this.newPostData}
-                </div>
+    return (
+        <div className={s.my__posts}>
+            <div className={s.header}>My posts</div>
+            <NewPostForm onSubmit={onSubmit}/>
+            <div className={s.posts}>
+                {newPostData}
             </div>
-        )
-    }
-}
+        </div>
+    )
+
+})
 
 export default MyPosts;
